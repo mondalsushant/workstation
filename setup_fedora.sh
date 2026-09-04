6155047ecac9fa0b7536bcdc938e307ea9032ebe
Sushant Mondal <contact@sushantmondal.com>
2026-09-04 15:37:45 +0530

    feat(setup_fedora): add micro editor and remove sudo usage from vscode
    Signed-off-by: Sushant Mondal <contact@sushantmondal.com>

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -74,10 +74,21 @@ sensors-detect --auto
 # Visual Studio Code
 # ++++++++++++++++++
 # Refs: https://code.visualstudio.com/docs/setup/linux#_install-vs-code-on-linux
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update &&
-sudo dnf install code # or code-insiders
+dnf install code # or code-insiders
+
+# `micro`
+# +++++++
+# Refs: https://github.com/micro-editor/micro#building-from-source
+dnf install -y golang
+mkdir -p /tmp/git
+cd /tmp/git
+git clone https://github.com/micro-editor/micro
+cd micro
+make build
+mv micro /usr/local/bin # optional
 
 # Though `nano` should already come pre-installed.
 dnf install -y nano neovim
